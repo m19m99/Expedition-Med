b6f5cd40bb312b82e45d25d3d99bb318302c65f8
Garnier Olivier <mr.ogarnier@gmail.com>
2023-05-13 05:57:31 +0200

    format mobile et foreach affichage zone plastique

--- map/main.js
+++ map/main.js
@@ -1,14 +1,33 @@
+/// Génération Map ///
+
+
 const LatLong = {
-  lat: 43.2965,
-  long: 5.3698,
+  lat: 35.8575,
+  long: 18.8530,
 };
 
-const zoom = 5;
+const zoom = 3;
 
 let map = L.map("map").setView([LatLong.lat, LatLong.long], zoom);
 
+let mediterraneanBounds = L.latLngBounds(
+  L.latLng(30, -20), // coin sud-ouest de la Méditerranée
+  L.latLng(46, 36) // coin nord-est de la Méditerranée
+);
+
+map.setMaxBounds(mediterraneanBounds);
+
+map.setMinZoom(3); 
+
+
+
+
+
+/// Fonction Convertion Coordonnées ///
+
+
 L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-  maxZoom: 19,
+  maxZoom: 10,
   attribution:
     '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 }).addTo(map);
@@ -32,15 +51,53 @@ function convertCoords(latitude, longitude) {
   };
 }
 
+
+
+
+/// ZONE PLASTIQUE ///
+
+
 // Exemple d'utilisation :
-let latitude = "42°08.214 N";
-let longitude = "11°36.646 E";
+let coordinates = [
+  { latitude: "42°08.214 N", longitude: "11°36.646 E" },
+  { latitude: "45°25.784 N", longitude: "12°20.643 E" },
+  { latitude: "41°53.716 N", longitude: "12°28.692 E" }
+];
 
-let coords = convertCoords(latitude, longitude);
+coordinates.forEach(function(coord) {
+  let coords = convertCoords(coord.latitude, coord.longitude);
 
-let circle = L.circle([coords.latitude, coords.longitude], {
-  color: "#1B3C60",
-  fillColor: "#1B3C60",
-  fillOpacity: 0.5,
-  radius: 30000,
-}).addTo(map);
+  let circle = L.circle([coords.latitude, coords.longitude], {
+    color: "#1B3C60",
+    fillColor: "#1B3C60",
+    fillOpacity: 0.5,
+    radius: 30000,
+  }).addTo(map);
+
+  circle.bindPopup("I am a circle.", {
+    className: "myPopup"
+  });
+});
+
+let popupContent = `
+  <div class="my-popup">
+    <h3>Mon titre de popup</h3>
+    <p>Je suis un contenu de popup personnalisé.</p>
+  </div>
+`;
+
+circle.bindPopup(popupContent);
+
+
+
+
+/// Point de prélévement ///
+
+
+// Définir les coordonnées de la polyligne
+let polylineCoords = [
+  
+];
+
+// Créer la polyligne
+let polyline = L.polyline(polylineCoords, {color: 'red'}).addTo(map);
