66a4153454d7ef4d24f5ba2a9bb31ad5fc0fc2f0
m19m199 <myriam.dagher1@gmail.com>
2023-05-13 02:10:00 +0200

    base de données: MCD, MLD,script sql

---
+++ base de données/script.sql
@@ -0,0 +1,84 @@
+CREATE DATABASE IF NOT EXISTS expeditionmed;
+
+CREATE USER IF NOT EXISTS 'sea'@'localhost' IDENTIFIED BY 'azerty1234';
+
+GRANT ALL PRIVILEGES ON expeditionmed.* TO 'sea'@'localhost';
+
+FLUSH PRIVILEGES;
+
+USE expeditionmed; 
+
+CREATE TABLE `Sample_DATA` (
+    `Sample` string  NOT NULL ,
+    `Sea` string  NOT NULL ,
+    `Start_date` date  NOT NULL ,
+    `Start_time` date  NOT NULL ,
+    PRIMARY KEY (
+        `Sample`
+    )
+);
+
+CREATE TABLE `Gps` (
+    `id` int  NOT NULL ,
+    `start_latitude` int  NOT NULL ,
+    `start_longitude` int  NOT NULL ,
+    `mid_latitude` int  NOT NULL ,
+    `mid_longitude` int  NOT NULL ,
+    `end_latitude` int  NOT NULL ,
+    `end_longitude` int  NOT NULL ,
+    `Sample` string  NOT NULL ,
+    PRIMARY KEY (
+        `id`
+    )
+);
+
+CREATE TABLE `Navigation` (
+    `id` int  NOT NULL ,
+    `wind_force` int  NOT NULL ,
+    `wind_speed` int  NOT NULL ,
+    `sea_state` int  NOT NULL ,
+    `water_temperature` int  NOT NULL ,
+    `Sample` string  NOT NULL ,
+    PRIMARY KEY (
+        `id`
+    )
+);
+
+CREATE TABLE `Surface` (
+    `id` int  NOT NULL ,
+    `boat_speed` int  NOT NULL ,
+    `start_flowmeter` int  NOT NULL ,
+    `filtered_volume` int  NOT NULL ,
+    `filtered_distance` int  NOT NULL ,
+    `filtered_surface` int  NOT NULL ,
+    `comments` string  NOT NULL ,
+    `Sample` string  NOT NULL ,
+    PRIMARY KEY (
+        `id`
+    )
+);
+
+CREATE TABLE `Tri` (
+    `id` int  NOT NULL ,
+    `size` string  NOT NULL ,
+    `type` string  NOT NULL ,
+    `color` int  NOT NULL ,
+    `number` int  NOT NULL ,
+    `Sample` string  NOT NULL ,
+    PRIMARY KEY (
+        `id`
+    )
+);
+
+ALTER TABLE `Gps` ADD CONSTRAINT `fk_Gps_Sample` FOREIGN KEY(`Sample`)
+REFERENCES `Sample_DATA` (`Sample`);
+
+ALTER TABLE `Navigation` ADD CONSTRAINT `fk_Navigation_Sample` FOREIGN KEY(`Sample`)
+REFERENCES `Sample_DATA` (`Sample`);
+
+ALTER TABLE `Surface` ADD CONSTRAINT `fk_Surface_Sample` FOREIGN KEY(`Sample`)
+REFERENCES `Sample_DATA` (`Sample`);
+
+ALTER TABLE `Tri` ADD CONSTRAINT `fk_Tri_Sample` FOREIGN KEY(`Sample`)
+REFERENCES `Sample_DATA` (`Sample`);
+
